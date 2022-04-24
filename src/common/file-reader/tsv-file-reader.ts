f87a7e895ba23f274d38accf24b756a0ee5df013
ilya53nov <ilya53nov@yandex.ru>
2022-04-24 13:33:21 +0300

    Добавлено чтение из файла с использованием потоков

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,64 +1,37 @@
-import { readFileSync } from 'fs';
+import EventEmitter from 'events';
+import { createReadStream } from 'fs';
 import { UTF_8 } from '../../const.js';
-import { FilmGenre } from '../../types/film-genre.enum.js';
-import { Film } from '../../types/film.type.js';
+import { EmitEvent } from './emit-event.enum.js';
 import { FileReaderInterface } from './file-reader.interface.js';
 
-export default class TSVFileReader implements FileReaderInterface {
-  private rawData = '';
-
-  constructor(public filename: string) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: UTF_8});
+export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
+  constructor(public filename: string) {
+    super();
   }
 
-  public toArray(): Film[] {
-    if (!this.rawData) {
-      return [];
+  public async read():Promise<void> {
+    const stream = createReadStream(this.filename, {
+      highWaterMark: 2 ** 14,
+      encoding: UTF_8,
+    });
+
+    let lineRead = '';
+    let endLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of stream) {
+      lineRead += chunk.toString();
+
+      while ((endLinePosition = lineRead.indexOf('\n')) >= 0) {
+        const completeRow = lineRead.slice(0, endLinePosition + 1);
+        lineRead = lineRead.slice(++endLinePosition);
+        importedRowCount++;
+
+        this.emit(EmitEvent.Line, completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map(([
-        title,
-        description,
-        publicationDate,
-        genre,
-        year,
-        rating,
-        previewVideoLink,
-        videoLink,
-        actors,
-        director,
-        runTime,
-        commentsCount,
-        name,
-        email,
-        avatarPath,
-        password,
-        posterImage,
-        backgroundImage,
-        backgroundColor
-      ]) => ({
-        title,
-        description,
-        publicationDate: new Date(publicationDate),
-        genre: FilmGenre[genre as FilmGenre] ,
-        year: Number.parseInt(year, 10),
-        rating: Number.parseInt(rating, 10),
-        previewVideoLink,
-        videoLink,
-        actors: actors.split(';'),
-        director,
-        runTime: Number.parseInt(runTime, 10),
-        commentsCount: Number.parseInt(commentsCount, 10),
-        user: {name, email, avatarPath, password},
-        posterImage,
-        backgroundImage,
-        backgroundColor,
-      }));
+    this.emit(EmitEvent.End, importedRowCount);
   }
+
 }
