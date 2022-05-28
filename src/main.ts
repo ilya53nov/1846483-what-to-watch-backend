0abe13abd42879aad976ddd8df7191004b4e4266
ilya53nov <ilya53nov@yandex.ru>
2022-05-28 14:11:13 +0300

    В контейнер добавлены: фильтр исключений, контроллеры

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,20 @@ import { Container } from 'inversify';
 import Application from './app/application.js';
 import { ConfigInterface } from './common/config/config.interface.js';
 import ConfigService from './common/config/config.service.js';
+import { ControllerInterface } from './common/controller/controller.interface.js';
 import { DatabaseInterface } from './common/database-client/database.interface.js';
 import DatabaseService from './common/database-client/database.service.js';
+import { ExceptionFilterInterface } from './common/errors/exception-filter.interface.js';
+import ExceptionFilter from './common/errors/exception-filter.js';
 import { LoggerInterface } from './common/logger/logger.interface.js';
 import LoggerService from './common/logger/logger.service.js';
+import FavoriteFilmController from './modules/film/favorite-film.controller.js';
 import { FilmServiceInterface } from './modules/film/film-service.interface.js';
+import FilmController from './modules/film/film.controller.js';
 import { FilmEntity, FilmModel } from './modules/film/film.entity.js';
 import FilmService from './modules/film/film.service.js';
 import { UserServiceInterface } from './modules/user/user-service.interface.js';
+import UserController from './modules/user/user.controller.js';
 import { UserEntity, UserModel } from './modules/user/user.entity.js';
 import UserService from './modules/user/user.service.js';
 import { Component } from './types/component.types.js';
@@ -25,7 +31,10 @@ applicationContainer.bind<UserServiceInterface>(Component.UserServiceInterface).
 applicationContainer.bind<ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 applicationContainer.bind<FilmServiceInterface>(Component.FilmServiceInterface).to(FilmService);
 applicationContainer.bind<ModelType<FilmEntity>>(Component.FilmModel).toConstantValue(FilmModel);
-
+applicationContainer.bind<ExceptionFilterInterface>(Component.ExceptionFilterInterface).to(ExceptionFilter).inSingletonScope();
+applicationContainer.bind<ControllerInterface>(Component.UserController).to(UserController).inSingletonScope();
+applicationContainer.bind<ControllerInterface>(Component.FilmController).to(FilmController).inSingletonScope();
+applicationContainer.bind<ControllerInterface>(Component.FavoriteFilmController).to(FavoriteFilmController).inSingletonScope();
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
